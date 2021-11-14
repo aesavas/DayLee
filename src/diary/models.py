b8e2df07a4e3e8d4d4a829afca6860ef9c47b14f
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2021-11-14 19:47:12 +0000

    Diary model created.

--- src/diary/models.py
+++ src/diary/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import uuid
+import datetime
 
-# Create your models here.
+from users.models import Profile
+
+class Diary(models.Model):
+    MOOD_TYPE = {
+        ('happy', 'Happy'),
+        ('sad', 'Sad'),
+        ('angry', 'Angry'),
+    }
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.CASCADE)
+    diary = models.TextField(null=True, blank=True)
+    mood = models.CharField(max_length=200, choices=MOOD_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return datetime.datetime.strftime(self.created, "%d-%B-%Y %A")
